a89cf8e20332eae3da879079de6392e11fcf8f2f
Sai Rohith Reddy Vangala <sairohit.vangala@vannainfotech.com>
2022-06-08 20:41:05 +0530

    Update shell script

--- create_release.sh
+++ create_release.sh
@@ -1,7 +1,8 @@
-rls_date_current = date + '%Y.%m.%d'
-rls_branch = 'release/${rls_date_current}.001'
+rls_date_current=$(date +'%Y.%m.%d')
+echo ${rls_date_current}
+rls_branch=release/${rls_date_current}.001
 echo "Creating the release branch ${rls_branch}"
 
 git checkout develop
-git checkout -b rls_branch
-git push rls_branch
+git checkout -b ${rls_branch}
+git push ${rls_branch}
