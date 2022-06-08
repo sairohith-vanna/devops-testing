ad2bbf9cd55647307695f371716d27132001eb0a
Sai Rohith Reddy Vangala <sairohit.vangala@vannainfotech.com>
2022-06-08 20:21:07 +0530

    Adds a script to create release branch

---
+++ create_release.sh
@@ -0,0 +1,7 @@
+rls_date_current = date + '%Y.%m.%d'
+rls_branch = 'release/${rls_date_current}.001'
+echo "Creating the release branch ${rls_branch}"
+
+git checkout develop
+git checkout -b rls_branch
+git push rls_branch
